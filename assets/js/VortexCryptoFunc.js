be3256b02756c6d3b99d715b4238685fd4a5431e
rezasabz <rezasabz21@gmail.com>
2022-01-24 19:29:51 +0330

    New Project Version

---
+++ assets/js/VortexCryptoFunc.js
@@ -0,0 +1,11 @@
+// Cryptograohic functions for the Vortex application
+// معادل سازی اولیه های رمزنگاری بصورت کلاس و آبجکت جهت استفاده در پارسر
+
+class Encrypt {
+    key = "";
+    // constructor() {
+    //     this.key = key;
+    //     // this.plainText = plainText;
+    //     // this.cipherText = cipherText;
+    // }
+}
